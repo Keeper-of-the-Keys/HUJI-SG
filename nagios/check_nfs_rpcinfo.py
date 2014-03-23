0b8be857123a8080eba902b146f17490812f2647
Keeper-of-the-Keys <esr+huji-sg-git@mail.hebrew.edu>
2014-03-23 15:30:41 +0200

    Adds a basic NFS check (using only information available through rpcinfo -p) Adds beginning of a basic Nagios library
    TODO: move other scripts to nagios library
    TODO: add more/better NFS functionality tests

---
+++ nagios/check_nfs_rpcinfo.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+#################################################### 
+##  ___   ____       ____   ___  ## cs.huji.ac.il ##
+## |   | /   /       \   \ |   | ##    /\         ##
+## |   |/   /         \   \|   | ##   / /         ##
+## |       /       __  \       | ##   \_/         ##
+## |      /   ___ |  |_ \      | ##    _          ##
+## |      \  / _ \|   _|/      | ##   | |  __  _  ##
+## |       \/ / \ \  | /       | ##   | | /  \//  ##
+## |   |\   \ \_/ /  |/   /|   | ##   | |/ /\ \   ##
+## |___| \___\___/\__/___/ |___| ##   \_  /  \_\  ##
+##      KeeperoftheKeys.nl       ##    /_/        ## 
+####################################################
+
+###############################################################################
+# Written by E.S. Rosenberg 5774/2014
+#
+# This script will try to ascertain whether all processes needed for NFS are 
+# running on the NFS server, it uses rpcinfo which is called through the python
+# subprocess module.
+#
+# It also depends on the HUJI nagios library
+#
+# @author: E.S. Rosenberg (Keeper of the Keys) <esr at mail dot hebrew dot edu>
+# @license: GPLv2
+###############################################################################
+
+### Imports ###
+try:
+    import nagios
+    import argparse
+    from subprocess import check_output
+except Exception as e:
+    print "UNKNOWN: " + str(e)
+    nagios.set_exit(nagios.exit_codes['unknown'])
+    exit(nagios.exit_code)
+
+### End Imports ###
+
+def check_procs(host):
+    process_list = ["nfs", "mountd", "portmapper", "nlockmg"]
+
+    rpcinfo_output = check_output(["rpcinfo", "-p", host])
+
+    for process in process_list:
+        if(rpcinfo_output.find(process) == -1):
+            nagios.set_exit(nagios.exit_codes['critical'])
+            nagios.string_results += ['process: {} not found in RPC'.format(
+                process
+            )]
+
+    nagios.set_exit(nagios.exit_codes['ok'])
+    if(nagios.exit_code == nagios.exit_codes['ok']):
+        nagios.string_results += ['All processes required for NFS found']
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(__file__)
+
+    parser.add_argument('-H', '--host', help = 'Hostname or IP address, if no ' +
+                        'other arguments are specified all available info ' +
+                        'will be printed', dest = 'host', required = True)
+
+    args = parser.parse_args()
+
+    try:
+        check_procs(args.host)
+    except Exception as e:
+        print "UNKNOWN: " + str(e)
+        nagios.set_exit(nagios.exit_codes['unknown'])
+        exit(nagios.exit_code)
+
+    nagios.output_and_exit()
---
+++ nagios/nagios.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+#################################################### 
+##  ___   ____       ____   ___  ## cs.huji.ac.il ##
+## |   | /   /       \   \ |   | ##    /\         ##
+## |   |/   /         \   \|   | ##   / /         ##
+## |       /       __  \       | ##   \_/         ##
+## |      /   ___ |  |_ \      | ##    _          ##
+## |      \  / _ \|   _|/      | ##   | |  __  _  ##
+## |       \/ / \ \  | /       | ##   | | /  \//  ##
+## |   |\   \ \_/ /  |/   /|   | ##   | |/ /\ \   ##
+## |___| \___\___/\__/___/ |___| ##   \_  /  \_\  ##
+##      KeeperoftheKeys.nl       ##    /_/        ## 
+####################################################
+
+###############################################################################
+# Written by E.S. Rosenberg 5774/2014
+#
+# Library with some basic nagios related stuff, that kept repeating itself.
+#
+# @author: E.S. Rosenberg (Keeper of the Keys) <esr at mail dot hebrew dot edu>
+# @license: GPLv2
+###############################################################################
+
+exit_codes = dict()
+exit_codes['ok'] = 0
+exit_codes['warning'] = 1
+exit_codes['critical'] = 2
+exit_codes['unknown'] = 3
+exit_code = exit_codes['unknown']
+
+string_results = []
+string_perfdata = []
+
+def set_exit(status):
+    global exit_code
+    if exit_code == exit_codes['unknown']:
+        if status < exit_code:
+            exit_code = status
+    else:
+        if status > exit_code:
+            exit_code = status
+
+def output_and_exit():
+    global exit_code, exit_codes, string_results, string_perfdata
+
+    if len(string_results) == 1 and len(string_perfdata) > 0:
+        pipe_char = '\n|'
+    else:
+        pipe_char = ''
+
+#    if exit_code == exit_codes['critical']:
+#        print "CRITICAL: " + string_results[0] + pipe_char
+#    elif exit_code == exit_codes['warning']:
+#        print "WARNING: " + string_results[0] + pipe_char
+#    elif exit_code == exit_codes['ok']:
+#        print "OK: " + string_results[0] + pipe_char
+#    else:
+#        print "UNKNOWN"
+
+    if exit_code == exit_codes['critical']:
+        output = "CRITICAL: " #+ string_results[0] + pipe_char
+    elif exit_code == exit_codes['warning']:
+        output = "WARNING: " #+ string_results[0] + pipe_char
+    elif exit_code == exit_codes['ok']:
+        output = "OK: " #+ string_results[0] + pipe_char
+    else:
+        output = "UNKNOWN"
+
+    if len(string_results) >= 1:
+        output += string_results[0] + pipe_char
+
+    print output
+ 
+    for result in string_results[1:-1]:
+        print result
+    
+    if len(string_perfdata) > 0:
+        if len(string_results) > 1:
+            print string_results[-1] + "|"
+
+        for perfdata in string_perfdata:
+            print perfdata
+    elif len(string_results) > 1:
+        print string_results[-1]
+
+    exit(exit_code)
